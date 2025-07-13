e74006c9c0323fd3a0f385949e5cce5ee9355e5e
Devendra <devendra.991023@gmail.com>
2025-07-14 02:55:48 +0530

    roboshop script added

--- roboshop_automate.sh
+++ roboshop_automate.sh
@@ -10,12 +10,13 @@ do
     Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-039ef9c300b06ac00 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceID" --output text)
     if [ $instance != "frontend" ]
         then
-            IP=$(aws ec2 describe-instances --Intsance-ids$Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+            IP=$(aws ec2 describe-instances --Intsance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
             RECORD_NAME="$instance.$DOMAIN_NAME"
         else
-            IP=$(aws ec2 describe-instances --Intsance-ids$Instance_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+            IP=$(aws ec2 describe-instances --Intsance-ids $Instance_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
             RECORD_NAME="$DOMAIN_NAME"
     fi
+
     echo "$instance IP Address : $IP"
     
     aws route53 change-resource-record-sets \
