938061a8583ab7215c3e9d1d599e7485491f8003
Devendra <devendra.991023@gmail.com>
2025-07-14 02:40:12 +0530

    roboshop script added

---
+++ roboshop_automate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-039ef9c300b06ac00"
+ZONE_ID=Z0608002VA9OF3SBWG2W"
+DOMAIN_NAME="devdsoprac.site"
+
+for instance in $@
+do 
+    Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-039ef9c300b06ac00 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceID" --output text)
+    if [ $instance != "frontend" ]
+        then
+            IP=$(aws ec2 describe-instances --Intsance-ids$Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        else
+            IP=$(aws ec2 describe-instances --Intsance-ids$Instance_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instance IP Address : $IP"
+    
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action":   "UPSERT"
+        ,"ResourceRecordSet": {
+            "Name"          : "'$instance'.'$DOMAIN_NAME'"
+            ,"Type"         : "A"
+            ,"TTL"          : 1
+            ,"ResourceRecords" : [{
+                "Value"        : "'$IP'"
+            }]
+        }
+        }]
+    }'
+done
+    
