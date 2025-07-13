298c4de5ae5baa4c71fe626c6343b8f44ab85534
Devendra <devendra.991023@gmail.com>
2025-07-14 03:04:07 +0530

    roboshop script added

--- roboshop_automate.sh
+++ roboshop_automate.sh
@@ -7,13 +7,13 @@ DOMAIN_NAME="devdsoprac.site"
 
 for instance in $@
 do 
-    Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-039ef9c300b06ac00 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceID" --output text)
+    Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-039ef9c300b06ac00 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
         then
-            IP=$(aws ec2 describe-instances --Intsance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+            IP=$(aws ec2 describe-instances --intsance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
             RECORD_NAME="$instance.$DOMAIN_NAME"
         else
-            IP=$(aws ec2 describe-instances --Intsance-ids $Instance_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+            IP=$(aws ec2 describe-instances --intsance-ids $Instance_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
             RECORD_NAME="$DOMAIN_NAME"
     fi
 
@@ -23,11 +23,11 @@ do
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
-        "Comment": "creating or Updating a record set for cognito endpoint"
+        "Comment": "Creating or Updating a record set for cognito endpoint"
         ,"Changes": [{
         "Action":   "UPSERT"
         ,"ResourceRecordSet": {
-            "Name"          : "'$instance'.'$DOMAIN_NAME'"
+            "Name"          : "'$RECORD_NAME'"
             ,"Type"         : "A"
             ,"TTL"          : 1
             ,"ResourceRecords" : [{
