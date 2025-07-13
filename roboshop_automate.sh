5aac9dee6e12035558b7b1c650a039ef30403232
Devendra <devendra.991023@gmail.com>
2025-07-14 03:11:59 +0530

    roboshop script added

--- roboshop_automate.sh
+++ roboshop_automate.sh
@@ -23,18 +23,18 @@ do
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
-    {{
+    {
         "Comment": "Creating or Updating a record set for cognito endpoint"
         ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
+        "Action"                : "UPSERT"
+        ,"ResourceRecordSet"    : {
             "Name"              : "'$RECORD_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
+            "Value"         : "'$IP'"
             }]
         }
         }]
-    }'
+     }'
 done
